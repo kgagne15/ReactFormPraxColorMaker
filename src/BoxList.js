a2af96fb37b3cb6d29654a974c6ec6a7c6ec49a8
kgagne15 <kristinagagne@ymail.com>
2022-10-08 12:08:23 -0400

    overall format highlighted

--- src/BoxList.js
+++ src/BoxList.js
@@ -2,9 +2,7 @@ import React from "react";
 
 const BoxList = () => {
     return (
-        <div>
-            I am a div
-        </div>
+        <NewBoxForm />
     )
 }
 
---
+++ src/NewBoxForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewBoxForm = () => {
+
+
+    return (
+        <Box />
+    )
+}
+
+export default NewBoxForm;
